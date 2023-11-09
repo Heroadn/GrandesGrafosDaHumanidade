847dda0427c62c2ca9375316319889a7fb0c69aa
heroadn <benbenjamin554@gmail.com>
2023-11-09 12:42:18 -0300

    Initial setup

---
+++ Main.py
@@ -0,0 +1,5 @@
+from src.Node import Node;
+
+if __name__ == "__main__":
+    node = Node()
+    node.say_hello()
---
+++ src/Node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self):
+        self.id = 'xD'
+
+    def say_hello(self):
+        print("TESTE: ", self.id)

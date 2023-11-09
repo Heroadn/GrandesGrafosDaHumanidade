dab778404ed42903bfc269756adc7ede927e075e
heroadn <benbenjamin554@gmail.com>
2023-11-09 15:58:18 -0300

    start

--- Main.py
+++ Main.py
@@ -1,5 +1,13 @@
-from src.Node import Node
+from src.Edge import Edge
+from src.Graph import Graph
+from src.TreeLib.Node import Node
 
 if __name__ == "__main__":
-    node = Node()
-    node.to_string()
+    edge1 = Edge("a", "b", 10)
+    edge2 = Edge("b", None, 10)
+
+    graph = Graph()
+    graph.add_edge(edge1)
+    graph.add_edge(edge2)
+
+    graph.print_graph()

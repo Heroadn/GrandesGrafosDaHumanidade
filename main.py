f56110382e21fb88cf94f873d496b5bd90baedd3
Thiago do Nascimento <venancio862@gmail.com>
2023-11-10 22:14:18 -0300

    Reading from CSV now

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import csv
+
 from src.edge import Edge
 from src.graph import Graph
 
-if __name__ == "__main__":
-    edge1 = Edge("cidade_a", "cidade_b", 10)
-    edge2 = Edge("cidade_a", "cidade_c", 20)
-    edge3 = Edge("cidade_c", "cidade_d", 20)
-    edge4 = Edge("cidade_c", "cidade_e", 10)
 
+def csv_to_graph(csv_file_path: str) -> Graph:
     graph = Graph()
-    graph.add_edge(edge1)
-    graph.add_edge(edge2)
-    graph.add_edge(edge3)
-    graph.add_edge(edge4)
+
+    with open(csv_file_path, newline='') as csvfile:
+        csv_content = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csv_content:
+            if len(row) >= 3:
+                if row[2].isdigit(): #Verifying if distance field is a valid digit to be converted to integer
+                    graph.add_edge(Edge(row[0], row[1], int(row[2])))
+
+    return graph
+
+
+
+if __name__ == "__main__":
+    graph = csv_to_graph("paths.csv")
 
     graph.print_graph()
     graph.dijkstra("cidade_a", "cidade_d")
 
-    graph.dijkstra("cidade_a", "cidade_e")
+    graph.dijkstra("cidade_a", "cidade_f")

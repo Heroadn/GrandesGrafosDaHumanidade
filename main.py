3db716559592d38ec74b97d12cc679712f005019
Thiago do Nascimento <venancio862@gmail.com>
2023-11-11 00:08:04 -0300

    Adding bidirectional support

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     graph = csv_to_graph("paths.csv")
 
     graph.print_graph()
-    graph.dijkstra("cidade_a", "cidade_d")
 
+    graph.dijkstra("cidade_a", "cidade_d")
     graph.dijkstra("cidade_a", "cidade_f")
+    graph.dijkstra("cidade_f", "cidade_a")
--- src/graph.py
+++ src/graph.py
@@ -9,6 +9,7 @@ class Graph:
     def __init__(self):
         self.adjacency_list = {}
 
+
     def add_edge(self, edge: Edge):
         if edge.source_id not in self.adjacency_list:
             self.adjacency_list[edge.source_id] = []
@@ -18,12 +19,17 @@ class Graph:
 
         self.adjacency_list[edge.source_id].append(edge)
 
+        #Will also add the same path in reverse order, in order for the path to be bidirectional
+        self.adjacency_list[edge.destination_id].append(Edge(edge.destination_id, edge.source_id, edge.weight))
+
+
     def print_graph(self):
         i = 1
         for k in self.adjacency_list.keys():
             for edge in self.adjacency_list[k]:
                 print(f"vertex-{i} ({edge.source_id}) is connected to {edge.destination_id} with weight {edge.weight}")
             i+=1
+
 
     def dijkstra(self, start_vertex, end_vertex):
         priority_queue = [start_vertex]
@@ -59,18 +65,27 @@ class Graph:
         print("#"*55)
         self.print_shortest_path(start_vertex, end_vertex, distance, predecessor)
 
+
+    def print_along_with_dashes(self, number_of_dashes: int, content: str):
+        print("#"*number_of_dashes)
+        print(content)
+        print("#"*number_of_dashes)
+
+
     def print_shortest_path(self, start_vertex, end_vertex, distance, predecessor):
         path = []
         current_vertex = end_vertex
 
         while current_vertex != start_vertex:
             path.insert(0, current_vertex)
+            if current_vertex not in predecessor:
+                break
             current_vertex = predecessor[current_vertex]
 
         path.insert(0, start_vertex)
 
-        print("#"*55)
-        print(f"Shortest path from vertex {start_vertex} to vertex {end_vertex}: {' -> '.join(path)}")
-        print("#"*55)
-        print(f"Shortest distance from vertex {start_vertex} to vertex {end_vertex}: {distance[end_vertex]}")
-        print("#"*55)
+        if end_vertex in distance and distance[end_vertex] != float('inf'):
+            self.print_along_with_dashes(55, f"Shortest path from vertex {start_vertex} to vertex {end_vertex}: {' -> '.join(path)}")
+            self.print_along_with_dashes(55, f"Shortest distance from vertex {start_vertex} to vertex {end_vertex}: {distance[end_vertex]}")
+        else:
+            self.print_along_with_dashes(55, f"Couldn't find a path from {start_vertex} to {end_vertex}!")

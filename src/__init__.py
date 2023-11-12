a94c27220ca1133bbe55fb4dc8f831aa0fc42c4a
Thiago do Nascimento <venancio862@gmail.com>
2023-11-12 15:41:37 -0300

    More adjusts on the structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,9 @@
 import os
-import csv
 
 from flask import Flask, current_app
 
 from src.controller import main_bp
-from src.classes.edge import Edge
-from src.classes.graph import Graph
-
-def csv_to_graph(csv_file_path: str) -> Graph:
-    graph = Graph()
-
-    with open(csv_file_path, newline='') as csvfile:
-        csv_content = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in csv_content:
-            if len(row) >= 3:
-                if row[2].isdigit(): #Verifying if distance field is a valid digit to be converted to integer
-                    graph.add_edge(Edge(row[0], row[1], int(row[2])))
-
-    return graph
+from src.utils import csv_to_graph
 
 main_graph = csv_to_graph(os.path.join("resources", "paths.csv"))
 
@@ -26,5 +12,3 @@ app = Flask(__name__)
 app.main_graph = main_graph
 
 app.register_blueprint(main_bp)
-
-

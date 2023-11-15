faf3b1002ba7c487a6a7f950129deeae474ea408
Thiago do Nascimento <venancio862@gmail.com>
2023-11-15 00:56:55 -0300

    Added shortest_path route

--- Backend/pathfinder/demo/src/main/java/com/example/pathfinder/api/GraphResource.java
+++ Backend/pathfinder/demo/src/main/java/com/example/pathfinder/api/GraphResource.java
@@ -1,16 +1,22 @@
 package com.example.pathfinder.api;
 
 import com.example.pathfinder.graph.Graph;
+import com.example.pathfinder.graph.Path;
 import com.example.pathfinder.services.GraphService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/graph")
 public class GraphResource {
 
     private final GraphService graphService;
@@ -19,7 +25,7 @@ public class GraphResource {
         this.graphService = graphService;
     }
 
-    @GetMapping("/graph/test")
+    @GetMapping("/test")
     public String test() {
         Graph graph = graphService.getGraph();
         graph.printGraph();
@@ -28,7 +34,7 @@ public class GraphResource {
         return "Rota de teste do grafo principal! Deve imprimir o grafo no console da aplicação!";
     }
 
-    @GetMapping("/graph/print")
+    @GetMapping("/print")
     public Map<String, String> print() {
         Graph graph = graphService.getGraph();
         Map<String, String> response = new HashMap<>();
@@ -36,7 +42,7 @@ public class GraphResource {
         return response;
     }
 
-    @GetMapping("/graph/nodes")
+    @GetMapping("/nodes")
     public Map<String, LinkedList<String>> getNodes() {
         Graph graph = graphService.getGraph();
         Map<String, LinkedList<String>> response = new HashMap<>();
@@ -44,12 +50,29 @@ public class GraphResource {
         return response;
     }
 
-    @GetMapping("/graph/content")
+    @GetMapping("/content")
     public Map<String, Map<String, LinkedList<Map<String, Object>>>>  getContent() {
         Graph graph = graphService.getGraph();
         Map<String, Map<String, LinkedList<Map<String, Object>>>> response = new HashMap<>();
         response.put("graph", graph.getGraph());
         return response;
+    }
+
+    @GetMapping("/shortest_path/{pointA}/{pointB}")
+    public ResponseEntity<Map<String, Object>> getShortestPath(@PathVariable String pointA, @PathVariable String pointB) {
+        Graph graph = graphService.getGraph();
+        Path path_obj = graph.dijkstra(pointA, pointB);
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (path_obj == null){
+            response.put("msg", "Couldn't find a path from "+pointA+" to "+pointB);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
+        response.put("shortest_distance", path_obj.getDistance());
+        response.put("shortest_path", path_obj.getPaths());
+        return ResponseEntity.ok(response);
     }
 
 }
--- Backend/pathfinder/demo/src/main/java/com/example/pathfinder/api/HelloResource.java
+++ Backend/pathfinder/demo/src/main/java/com/example/pathfinder/api/HelloResource.java
@@ -1,27 +1,13 @@
 package com.example.pathfinder.api;
 
-import com.example.pathfinder.graph.Graph;
-import com.example.pathfinder.services.GraphService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
-
-    private final GraphService graphService;
-
-    public HelloResource(GraphService graphService) {
-        this.graphService = graphService;
-    }
-
     @GetMapping("/")
     public String index() {
-        //The following lines are just intended to test the application for a while,
-        // they should be removed later
-        Graph graph = graphService.getGraph();
-        graph.printGraph();
-        return "Salve salve! Essa rota é só pra ver se tá tudo funcionando de fato";
+        return "Здравствуйте мои товарищ! Как дела? Вся власть Советам!";
     }
 
 }
